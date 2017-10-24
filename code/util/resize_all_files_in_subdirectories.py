9b40bc9edf477779dfeb1f2ca92f2f7e3de1748e
Szymon Stoma <xxx@xxx>
2017-10-24 18:25:27 +0200

    * cnt

--- code/util/resize_all_files_in_subdirectories.py
+++ code/util/resize_all_files_in_subdirectories.py
@@ -1,19 +1,66 @@
-"""
+#!/usr/bin/env python
+__doc__ = """
 Script resizes all images in the folder. Parameters which can be specified by the user.
 scale (default = 0.5) <= how much we downscale images e.g 0.5 makes images of 1/4 or area of orig.
 modify (default = True) <= are images changed in place (origs are overwritten)
 path (default = '.') <= where to look for images (with subdirectories)
 
-requires python-3.4
-"""
+Requires to install xvfb to not block the screen
 
+requires python-3.2
+"""
+__version__ = 0.1
+
+# impoorts
 import os
 from fnmatch import fnmatch
+import argparse
+import subprocess
 
-root = '/some/directory'
-pattern = "*.py"
+# ----------------------------------------------------------- conf
 
+conf_fiji_args = ['--headless', '-batch'] 
+
+# ----------------------------------------------------------- parsing args
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--scale', 
+	type=float,
+	help='how much we downscale images e.g 0.5 makes images of 1/4 or area of orig.',
+	default=0.5
+)
+parser.add_argument('--modify', 
+	help='are images changed in place (origs are overwritten)?',
+	default=True
+)
+parser.add_argument('--path', 
+	help='where to look for images (with subdirectories)',
+	default='.'
+)
+parser.add_argument('--file_regexp', 
+	help='what files to include?',
+	default='*'
+)
+parser.add_argument('--fiji_path', 
+	help='path to executable fiji',
+	default='fiji'
+)
+parser.add_argument('--version', action='version', version='%(prog)s '+str(__version__))
+
+args = parser.parse_args()
+
+# ----------------------------------------------------------- getting info about files
+"""
+root = args.path
+pattern = args.file_regexp
+
+to_process = []
 for path, subdirs, files in os.walk(root):
 	for name in files:
 		if fnmatch(name, pattern):
-			print os.path.join(path, name)
+			to_process.append(os.path.join(path, name))
+"""
+# print(to_process)
+
+# ----------------------------------------------------------- converting files
+
+subprocess.run([args.fiji_path, *conf_fiji_args]) 
